d3edb20a900a39a3ca682bad4d506cc47d83c2e2
IcaroCG <113849721+IcaroCG@users.noreply.github.com>
2022-09-20 18:54:26 +0000

    add Funcionario.h

---
+++ Funcionario.hpp
@@ -0,0 +1,65 @@
+#ifndef USUARIO_H
+#define USUARIO_H
+
+#include "Pessoa.h"
+#include "Data.h"
+#include <string>
+
+/**
+ * @brief Classe que armazena e gerencia as informacoes do funcionário.
+ *
+ */
+class Funcionario: public Pessoa {
+ public:
+  /**
+   * @brief Construtor padrao que inicializa todas as variaveis privadas da
+   * classe.
+   *
+   * @param login Login do usuario
+   * @param password Password do usuario
+   */
+  Funcionario(std::string nome, std::string email, int cpf, Data nascimento, std::string endereco, int matricula): Pessoa(nome, email);
+
+  /**
+   * @brief Retorna o login do 
+   *
+   * @return std::string Login do usuario
+   */
+  std::string get() const;
+
+  /**
+   * @brief Retorna o password do usuario
+   *
+   * @return std::string Password do usuario
+   */
+  std::string getPassword() const;
+
+  /**
+   * @brief Atualiza o login do usuario
+   *
+   * @param login Novo login do usuario
+   */
+  void setLogin(const std::string& login);
+
+  /**
+   * @brief Atualiza o password do usuario
+   *
+   * @param login Novo password do usuario
+   */
+  void setPassword(const std::string& password);
+
+ private:
+  /**
+   * @brief Login do usuario
+   *
+   */
+  // TODO: declare aqui o login do usuario
+
+  /**
+   * @brief Password do usuario
+   *
+   */
+  // TODO: declare aqui o password do usuario
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "Funcionario.hpp"
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
+}
