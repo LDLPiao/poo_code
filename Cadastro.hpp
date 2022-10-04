d2ae3df3725b756f51c5a2a4648e7c3794033a3d
IcaroCG <113849721+IcaroCG@users.noreply.github.com>
2022-10-04 19:12:40 +0000

    nome Cadastros para Cadastro

---
+++ Cadastro.hpp
@@ -0,0 +1,17 @@
+#ifndef CADASTRO_H
+#define CADASTRO_H
+
+
+#include <iostream>
+#include <string.h>
+/**
+   * @brief Cadastros
+*/
+class Cadastro{
+ private:
+  /**
+   * @brief lista de ponteiro para os grupos de permissão  
+   * que está incluso
+   */
+  list<G>
+}
--- Cadastros.cpp
+++
@@ -1,1 +0,0 @@
-#include "Cadastros.hpp"
